c50ab7ff9d8930cdb3f67cfa4b0a0ef735095b25
huntercront <38252870+huntercront@users.noreply.github.com>
2020-05-23 13:03:39 +0300

    add structure

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var curentScreenTop = $(".logos-anim-block").offset().top - $(window).scrollTop();
+$('.logos-line-1').css({'transform' : 'translate(' + 0 +', ' + curentScreenTop/20 + '%)'})
+$('.logos-line-2').css({'transform' : 'translate(' + 0 +', ' + curentScreenTop/40 + '%)'})
+$('.logos-line-3').css({'transform' : 'translate(' + 0 +', ' + curentScreenTop/30 + '%)'})
+$(document).scroll(function() {
+	var offsetFromScreenTop = $(".logos-anim-block").offset().top - $(window).scrollTop();
+	if($(window).scrollTop() < $(".logos-anim-block").offset().top ){
+		$('.logos-line-1').css({'transform' : 'translate(' + 0 +', ' + offsetFromScreenTop/10 + '%)'})
+		$('.logos-line-2').css({'transform' : 'translate(' + 0 +', ' + offsetFromScreenTop/20 + '%)'})
+		$('.logos-line-3').css({'transform' : 'translate(' + 0 +', ' + offsetFromScreenTop/15 + '%)'})
+	}
+});
