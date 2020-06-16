bdbcb7f379e95c43df0df1df80b5f24e03aab9db
huntercront <38252870+huntercront@users.noreply.github.com>
2020-06-16 17:55:48 +0300

    add header style

--- js/script.js
+++ js/script.js
@@ -12,4 +12,23 @@ $(document).scroll(function() {
 	}
 });
 }
+
+$('.mobile-icon').on('click', function(e){
+	$('.hamRotate').addClass('active');
+})
+$('.has-child a').on('click', function(e){
+
+})
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
 // new SimpleBar($('.road-container')[0])
